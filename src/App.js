71838e70a14bb349a778a3a9576fdd7c107eeeb4
Ayar Hlaine <ayarhlaine.dev@gmail.com>
2020-05-07 16:44:58 +0630

    Added Loading Progress Bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React from 'react';
-
+import React,{useEffect} from 'react';
+import { useLoader } from './useLoader';
+import './App.scss';
 function App() {
+  const {progress,progressText} = useLoader();
+  const progressPercent = progress * 100;
+  const className = `Progress__Bar__Data__${progressPercent}`;
   return (
     <div className="App">
-    App
+      <div className={'Bar__Title__Container'}>
+        <h2 className={'Bar__Title'}>RX JS Progress Bar</h2>
+      </div>
+      <div className="Progress__Border">
+        <div className={`Progress__Bar ${className}`}>
+           { progressPercent == 100 && 'Successfully Hacked!'}
+        </div>
+      </div>
+      <div className="Percentage">
+        <p className={'Percentage__Text'}>{progressPercent} %</p>
+      </div>
+      <div className="Progress__Text__Container">
+        <p className="Progress__Text">
+          {progressText}
+        </p>
+      </div>
+      <div className="Load__Button__Container">
+        <button className={'Hack__Button'} id="load">
+          Click To Hack!
+        </button>
+      </div>
+      <div className="SocialLink__Container">
+        <h3>Made By: <a href={'https://facebook.com/developerayarhlaine'}>Ayar Hlaine</a></h3>
+      </div>
     </div>
   );
 }
---
+++ src/useLoader.js
@@ -0,0 +1,60 @@
+import {useEffect,useState} from 'react';
+
+import { of, fromEvent, from } from 'rxjs';
+
+import { fromFetch } from 'rxjs/fetch';
+import {
+  delay,
+  switchMapTo,
+  concatAll,
+  count,
+  scan,
+  withLatestFrom,
+  share,
+  switchMap, catchError
+} from 'rxjs/operators';
+
+const requestOne = of('Hacking Google ...').pipe(delay(500));
+const requestTwo = of('Hacking Facebook ...').pipe(delay(800));
+const requestThree = of('Hacking Micorsoft ...').pipe(delay(1100));
+const requestFour = of('Hacking Github ...').pipe(delay(1400));
+const requestFive = of('Hacking To Me ...').pipe(delay(1700));
+
+const observables = [
+  requestOne,
+  requestTwo,
+  requestThree,
+  requestFour,
+  requestFive
+];
+
+export const useLoader = () => {
+    const [progress,setProgress] = useState(0);
+    const [progressText,setProgressText] = useState('');
+    useEffect(() => {
+        const loadButton = document.getElementById('load');
+        const array$ = from(observables);
+        const requests$ = array$.pipe(concatAll());
+        const clicks$ = fromEvent(loadButton, 'click');
+        
+        const progress$ = clicks$.pipe(switchMapTo(requests$), share());
+        
+        const count$ = array$.pipe(count());
+        
+        const ratio$ = progress$.pipe(
+          scan(current => current + 1, 0),
+          withLatestFrom(count$, (current, count) => current / count)
+        );
+        const updateProgress = progressRatio => {
+            setProgress(progressRatio);
+        };
+        
+        clicks$.pipe(switchMapTo(ratio$)).subscribe(updateProgress);
+        
+        const progressSubscirption = progress$.subscribe((data) => setProgressText(data));
+        return () => {
+            progressSubscirption.unsubscribe();
+        }
+      },[])
+      return {progress,progressText};
+}
